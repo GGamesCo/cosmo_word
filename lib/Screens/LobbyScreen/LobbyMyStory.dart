ba2d253860bcb1f47e178d8433ca3c7bed0394ca
povstyany <gpovstyany@gmail.com>
2022-06-25 19:53:28 +0200

    Add levels cards

--- lib/Screens/LobbyScreen/LobbyMyStory.dart
+++ lib/Screens/LobbyScreen/LobbyMyStory.dart
@@ -8,23 +8,56 @@ class LobbyMyStory extends StatelessWidget{
 
   @override
   Widget build(BuildContext context){
-    return IntrinsicHeight(
-      child: Stack(
-        children: [
-          Container(
-            child: Image.asset('assets/images/lobby/lobby-my-story.png'),
-          ),
-          Center(
-            child: Padding(
-              padding: const EdgeInsets.only(top: 40),
-              child: MyStoryProgress(
-                progressCurrent: 2,
-                progressTotal: 3
-              ),
-            )
-          )
-        ]
+
+    return Container(
+      decoration: BoxDecoration(
+        image: DecorationImage(image: AssetImage('assets/images/lobby/lobby-my-story.png'), fit: BoxFit.fitWidth)
       ),
+      child: Padding(
+        padding: const EdgeInsets.all(15.0),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            StoryItemCard(imageFile: 'assets/images/backgrounds/green_tile.jpg', title: "Welcome party"),
+            StoryItemCard(imageFile: 'assets/images/backgrounds/beach_with_boat_tile.jpg', title: "Mystery iceland"),
+            StoryItemCard(imageFile: 'assets/images/backgrounds/jungles_tile.jpg', title: "Wild jungle"),
+            StoryItemCard(imageFile: 'assets/images/backgrounds/blue_tile.jpg', title: "Windy voyage")
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class StoryItemCard extends StatelessWidget {
+
+  final String imageFile;
+  final String title;
+
+  StoryItemCard({required this.imageFile, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: IntrinsicHeight(
+            child: Column(
+              children: [
+                ClipRRect(
+                  borderRadius: BorderRadius.all(Radius.circular(12)),
+                  child: Image.asset(imageFile)
+                ),
+                SizedBox(height: 5),
+                Container(
+                  child: Text(title, style: TextStyle(color: Color.fromRGBO(116, 126, 126, 1), fontFamily: 'Roboto'), textAlign: TextAlign.center)
+                )
+              ],
+            ),
+          ),
+        )
+      )
     );
   }
 }
