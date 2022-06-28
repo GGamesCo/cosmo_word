23209efde4624e17430c88a66ee85be7021bd448
povstyany <gpovstyany@gmail.com>
2022-06-28 12:26:23 +0200

    Add location completion

--- lib/Screens/LobbyScreen/LobbyMyStory.dart
+++ lib/Screens/LobbyScreen/LobbyMyStory.dart
@@ -1,3 +1,5 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 import '../../GameBL/Services/StoryLocationsService/StoryLocationModel.dart';
@@ -11,6 +13,7 @@ class LobbyMyStory extends StatefulWidget{
 
   late StoryLocationsService locationsService;
   late StoryStateService storyStateService;
+  late Map<int, LocationStatus> locationStatus = Map<int, LocationStatus>();
 
   LobbyMyStory(){
     locationsService = getIt.get<StoryLocationsService>();
@@ -26,6 +29,17 @@ class _LobbyMyStoryState extends State<LobbyMyStory> {
   @override
   void initState(){
     super.initState();
+
+    for(var loc in widget.locationsService.allLocations){
+      var locStatus = LocationStatus.opened;
+      if(loc.levels.reduce(max) < widget.storyStateService.currentLevelId){
+        locStatus = LocationStatus.completed;
+      }
+      if(loc.levels.reduce(min) > widget.storyStateService.currentLevelId){
+        locStatus = LocationStatus.locked;
+      }
+      widget.locationStatus[loc.id] = locStatus;
+    }
 
     setState(() {
       widget.locations = widget.locationsService.allLocations;
@@ -45,7 +59,11 @@ class _LobbyMyStoryState extends State<LobbyMyStory> {
           mainAxisAlignment: MainAxisAlignment.spaceEvenly,
           children: [
             for(var loc in widget.locations.take(4)) ...[
-              StoryItemCard(imageFile: "assets/images/backgrounds/${loc.backgroundFileName.replaceAll(".jpg", "_tile.jpg")}", title: loc.title),
+              StoryItemCard(
+                imageFile: "assets/images/backgrounds/${loc.backgroundFileName.replaceAll(".jpg", "_tile.jpg")}",
+                title: loc.title,
+                locationStatus: widget.locationStatus[loc.id]!,
+              ),
             ]
           ],
         ),
@@ -58,20 +76,21 @@ class StoryItemCard extends StatelessWidget {
 
   final String imageFile;
   final String title;
+  final LocationStatus locationStatus;
 
-  StoryItemCard({required this.imageFile, required this.title});
+  StoryItemCard({required this.imageFile, required this.title, required this.locationStatus});
 
   @override
   Widget build(BuildContext context) {
     return Expanded(
-      child: IntrinsicHeight(
-        child: Stack(
-          children: [
-            Container(
-              child: Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: IntrinsicHeight(
-                  child: Column(
+      child: Container(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: IntrinsicHeight(
+            child: Column(
+              children: [
+                IntrinsicHeight(
+                  child: Stack(
                     children: [
                       Container(/*
                         decoration: BoxDecoration(
@@ -90,23 +109,35 @@ class StoryItemCard extends StatelessWidget {
                           child: Image.asset(imageFile)
                         ),
                       ),
-                      SizedBox(height: 5),
-                      Container(
-                        child: Text(title, style: TextStyle(color: Color.fromRGBO(116, 126, 126, 1), fontFamily: 'Roboto'), textAlign: TextAlign.center)
-                      )
-                    ],
+                      if(locationStatus == LocationStatus.completed) ...[
+                        Align(
+                            alignment: Alignment.bottomCenter,
+                            child: SizedBox(
+                                width: 25,
+                                child: Image.asset('assets/images/widget/completedLocationMark.png')
+                            )
+                        )
+                      ],
+                      if(locationStatus == LocationStatus.locked) ...[
+                        Align(
+                            alignment: Alignment.bottomCenter,
+                            child: SizedBox(
+                                width: 25,
+                                child: Image.asset('assets/images/widget/lockedLocationMark.png')
+                            )
+                        )
+                      ],
+                    ]
                   ),
                 ),
-              )
-            ),/*
-            Center(
-              child: SizedBox(
-                width: 25,
-                child: Image.asset('assets/images/widget/lock.png')
-              )
-            )*/
-          ]
-        ),
+                SizedBox(height: 5),
+                Container(
+                  child: Text(title, style: TextStyle(color: Color.fromRGBO(116, 126, 126, 1), fontFamily: 'Roboto'), textAlign: TextAlign.center)
+                )
+              ],
+            ),
+          ),
+        )
       )
     );
   }
