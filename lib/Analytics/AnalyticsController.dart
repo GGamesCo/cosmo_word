ff8b8d00d0d5cee90b9c9b1deb9f6d1a5056c837
povstyany <gpovstyany@gmail.com>
2022-07-12 23:42:03 +0200

    Mixpanel integration

--- lib/Analytics/AnalyticsController.dart
+++ lib/Analytics/AnalyticsController.dart
@@ -1,18 +1,21 @@
 import 'package:appsflyer_sdk/appsflyer_sdk.dart';
+import 'package:cosmo_word/Analytics/MixpanelTracker.dart';
 import 'package:cosmo_word/GameBL/Common/UserController.dart';
 import 'package:cosmo_word/GameBL/UserStateController.dart';
 import 'package:cosmo_word/di.dart';
 import 'package:firebase_analytics/firebase_analytics.dart';
 import 'package:injectable/injectable.dart';
+import 'package:mixpanel_flutter/mixpanel_flutter.dart';
 
 @singleton
 class AnalyticsController{
   final UserController userController;
+  final MixpanelTracker mixpanelTracker;
   late UserStateController userStateController;
 
   late Map<String, Object> defaultParams;
 
-  AnalyticsController({required this.userController});
+  AnalyticsController({required this.userController, required this.mixpanelTracker});
 
   Future<void> initAsync() async{
     userStateController = getIt.get<UserStateController>();
@@ -23,8 +26,8 @@ class AnalyticsController{
       "sessionId": userController.sessionId,
     };
 
-    await FirebaseAnalytics.instance
-        .setDefaultEventParameters(defaultParams);
+    await mixpanelTracker.initAsync(userController.userId);
+    await FirebaseAnalytics.instance.setDefaultEventParameters(defaultParams);
 
     FirebaseAnalytics.instance.setUserId(id: userController.userId);
     FirebaseAnalytics.instance.setUserProperty(name: "sessionId",  value: userController.sessionId);
@@ -33,6 +36,7 @@ class AnalyticsController{
   Future<void> logEventAsync(String eventName, {Map<String, Object>? params}) async{
     var eventParams = await _wrapWithAmbientContextAsync(params);
     await FirebaseAnalytics.instance.logEvent(name: eventName, parameters: eventParams);
+    mixpanelTracker.track(eventName, params: eventParams);
   }
 
   Future<Map<String, Object>> _wrapWithAmbientContextAsync(Map<String, Object>? params) async {
--- lib/di.config.dart
+++ lib/di.config.dart
@@ -6,32 +6,33 @@
 
 import 'package:get_it/get_it.dart' as _i1;
 import 'package:injectable/injectable.dart' as _i2;
-import 'package:shared_preferences/shared_preferences.dart' as _i12;
+import 'package:shared_preferences/shared_preferences.dart' as _i13;
 
-import 'Analytics/AnalyticsController.dart' as _i18;
-import 'Flame/Common/SoundsController.dart' as _i13;
+import 'Analytics/AnalyticsController.dart' as _i19;
+import 'Analytics/MixpanelTracker.dart' as _i11;
+import 'Flame/Common/SoundsController.dart' as _i14;
 import 'GameBL/Common/Abstract/IBalanceController.dart' as _i3;
 import 'GameBL/Common/Abstract/IFlowRepository.dart' as _i5;
 import 'GameBL/Common/Abstract/ITimerController.dart' as _i7;
-import 'GameBL/Common/Abstract/IWordInputController.dart' as _i19;
+import 'GameBL/Common/Abstract/IWordInputController.dart' as _i20;
 import 'GameBL/Common/Abstract/IWordRepository.dart' as _i9;
 import 'GameBL/Common/BalanceController.dart' as _i4;
 import 'GameBL/Common/FlowRepository.dart' as _i6;
-import 'GameBL/Common/StageManager.dart' as _i22;
+import 'GameBL/Common/StageManager.dart' as _i23;
 import 'GameBL/Common/TimerController.dart' as _i8;
-import 'GameBL/Common/UserController.dart' as _i16;
-import 'GameBL/Common/WordInputController.dart' as _i20;
+import 'GameBL/Common/UserController.dart' as _i17;
+import 'GameBL/Common/WordInputController.dart' as _i21;
 import 'GameBL/Common/WordRepository.dart' as _i10;
-import 'GameBL/DI/Module.dart' as _i25;
-import 'GameBL/Lobby/LobbyStage.dart' as _i21;
-import 'GameBL/Services/StoryLevelsService/StoryLevelsService.dart' as _i14;
+import 'GameBL/DI/Module.dart' as _i26;
+import 'GameBL/Lobby/LobbyStage.dart' as _i22;
+import 'GameBL/Services/StoryLevelsService/StoryLevelsService.dart' as _i15;
 import 'GameBL/Services/StoryLocationsService/StoryLocationsService.dart'
-    as _i15;
-import 'GameBL/Services/UserStateService/UserStateService.dart' as _i17;
-import 'GameBL/TimeChallenge/RocketChallengeConfig.dart' as _i11;
-import 'GameBL/TimeChallenge/TimeAtackStage.dart' as _i24;
+    as _i16;
+import 'GameBL/Services/UserStateService/UserStateService.dart' as _i18;
+import 'GameBL/TimeChallenge/RocketChallengeConfig.dart' as _i12;
+import 'GameBL/TimeChallenge/TimeAtackStage.dart' as _i25;
 import 'GameBL/UserStateController.dart'
-    as _i23; // ignore_for_file: unnecessary_lambdas
+    as _i24; // ignore_for_file: unnecessary_lambdas
 
 // ignore_for_file: lines_longer_than_80_chars
 /// initializes the registration of provided dependencies inside of [GetIt]
@@ -43,40 +44,42 @@ _i1.GetIt $initGetIt(_i1.GetIt get,
   gh.factory<_i5.IFlowRepository>(() => _i6.FlowRepository());
   gh.factory<_i7.ITimerController>(() => _i8.TimerController());
   gh.factory<_i9.IWordRepository>(() => _i10.WordRepository());
-  gh.factory<_i11.RocketChallengeConfig>(() => _i11.RocketChallengeConfig());
-  gh.factoryAsync<_i12.SharedPreferences>(() => registerModule.prefs);
-  gh.singleton<_i13.SoundsController>(_i13.SoundsController());
-  gh.singleton<_i14.StoryLevelsService>(_i14.StoryLevelsService());
-  gh.singleton<_i15.StoryLocationsService>(_i15.StoryLocationsService());
-  gh.singleton<_i16.UserController>(_i16.UserController());
-  gh.singletonAsync<_i17.UserStateService>(() async => _i17.UserStateService(
-      sharedPreferences: await get.getAsync<_i12.SharedPreferences>()));
-  gh.singleton<_i18.AnalyticsController>(
-      _i18.AnalyticsController(userController: get<_i16.UserController>()));
-  gh.factory<_i19.IWordInputController>(() => _i20.WordInputController(
+  gh.singleton<_i11.MixpanelTracker>(_i11.MixpanelTracker());
+  gh.factory<_i12.RocketChallengeConfig>(() => _i12.RocketChallengeConfig());
+  gh.factoryAsync<_i13.SharedPreferences>(() => registerModule.prefs);
+  gh.singleton<_i14.SoundsController>(_i14.SoundsController());
+  gh.singleton<_i15.StoryLevelsService>(_i15.StoryLevelsService());
+  gh.singleton<_i16.StoryLocationsService>(_i16.StoryLocationsService());
+  gh.singleton<_i17.UserController>(_i17.UserController());
+  gh.singletonAsync<_i18.UserStateService>(() async => _i18.UserStateService(
+      sharedPreferences: await get.getAsync<_i13.SharedPreferences>()));
+  gh.singleton<_i19.AnalyticsController>(_i19.AnalyticsController(
+      userController: get<_i17.UserController>(),
+      mixpanelTracker: get<_i11.MixpanelTracker>()));
+  gh.factory<_i20.IWordInputController>(() => _i21.WordInputController(
       wordRepository: get<_i9.IWordRepository>(),
       balanceController: get<_i3.IBalanceController>(),
-      analyticsController: get<_i18.AnalyticsController>()));
-  gh.factory<_i21.LobbyStage>(
-      () => _i21.LobbyStage(soundsController: get<_i13.SoundsController>()));
-  gh.singleton<_i22.StageManager>(
-      _i22.StageManager(analyticsController: get<_i18.AnalyticsController>()));
-  gh.singletonAsync<_i23.UserStateController>(() async =>
-      _i23.UserStateController(
-          userStateService: await get.getAsync<_i17.UserStateService>(),
-          levelsService: get<_i14.StoryLevelsService>(),
+      analyticsController: get<_i19.AnalyticsController>()));
+  gh.factory<_i22.LobbyStage>(
+      () => _i22.LobbyStage(soundsController: get<_i14.SoundsController>()));
+  gh.singleton<_i23.StageManager>(
+      _i23.StageManager(analyticsController: get<_i19.AnalyticsController>()));
+  gh.singletonAsync<_i24.UserStateController>(() async =>
+      _i24.UserStateController(
+          userStateService: await get.getAsync<_i18.UserStateService>(),
+          levelsService: get<_i15.StoryLevelsService>(),
           flowRepository: get<_i5.IFlowRepository>(),
           balanceController: get<_i3.IBalanceController>(),
-          analyticsController: get<_i18.AnalyticsController>()));
-  gh.factoryAsync<_i24.TimeAtackStage>(() async => _i24.TimeAtackStage(
-      userStateController: await get.getAsync<_i23.UserStateController>(),
+          analyticsController: get<_i19.AnalyticsController>()));
+  gh.factoryAsync<_i25.TimeAtackStage>(() async => _i25.TimeAtackStage(
+      userStateController: await get.getAsync<_i24.UserStateController>(),
       wordRepository: get<_i9.IWordRepository>(),
-      wordInputController: get<_i19.IWordInputController>(),
+      wordInputController: get<_i20.IWordInputController>(),
       timerController: get<_i7.ITimerController>(),
-      challengeConfig: get<_i11.RocketChallengeConfig>(),
+      challengeConfig: get<_i12.RocketChallengeConfig>(),
       balanceController: get<_i3.IBalanceController>(),
-      soundsController: get<_i13.SoundsController>()));
+      soundsController: get<_i14.SoundsController>()));
   return get;
 }
 
-class _$RegisterModule extends _i25.RegisterModule {}
+class _$RegisterModule extends _i26.RegisterModule {}
