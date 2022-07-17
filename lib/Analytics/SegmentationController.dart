bfd2a08fa6ae683b7d44f36fff62c9316ab23963
gekas <gekas@i.ua>
2022-07-17 23:26:28 +0200

    Remove analytic from segmentationController

--- lib/Analytics/SegmentationController.dart
+++ lib/Analytics/SegmentationController.dart
@@ -1,18 +1,12 @@
 import 'dart:async';
 import 'dart:convert';
 
-import 'package:cosmo_word/Analytics/AnalyticEvent.dart';
-import 'package:cosmo_word/Analytics/AnalyticsController.dart';
 import 'package:injectable/injectable.dart';
 import 'package:http/http.dart' as http;
 
 @singleton
 class SegmentationController{
   String segmentationConfigUrl = "https://raw.githubusercontent.com/GGamesCo/configs/main/segmentationConfig.json";
-
-  final AnalyticsController _analyticsController;
-
-  SegmentationController(this._analyticsController);
 
   Completer initializeCompleter = Completer();
   Map<String, bool> segments = new Map<String, bool>();
@@ -21,7 +15,6 @@ class SegmentationController{
     while(!initializeCompleter.isCompleted){
       try{
         var response = await http.get(Uri.parse(segmentationConfigUrl));
-        _analyticsController.logEventAsync(AnalyticEvents.SEGMENTATION_LOADED, params: {"segmentation_response": response.body});
         segments = Map<String, bool>.from(jsonDecode(response.body));
       }catch(e){
         print("Error while evaluating segmentation. ${e} \nWait 10 seconds and retry.");
